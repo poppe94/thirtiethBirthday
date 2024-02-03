178e7caf7d89d40a88914a3ab04373906de7073a
Christian <c.poppe@online.de>
2024-02-04 00:11:08 +0100

    implement image slideshow

--- invitation_manager/models/content.py
+++ invitation_manager/models/content.py
@@ -7,5 +7,9 @@ class Content(models.Model):
 
 class ImageFile(models.Model):
     file_name = models.CharField(max_length=64)
-    image = models.ImageField(upload_to='content-images')
+    image = models.ImageField(upload_to='content-images')  # optimal size: 1104x621
+    order = models.IntegerField(default=0)
     content = models.ForeignKey(Content, on_delete=models.CASCADE, related_name="images")
+
+    class Meta:
+        ordering = ['order', 'id']
