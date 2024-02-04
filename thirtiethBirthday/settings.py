f48e51fa3d4f13097ab089cc2dba8303160ceaee
Christian <c.poppe@online.de>
2024-02-04 16:02:52 +0100

    change static dir settings

--- thirtiethBirthday/settings.py
+++ thirtiethBirthday/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = ""
+
 try:
     with open(BASE_DIR / 'key') as f:
         SECRET_KEY = f.read().strip()
@@ -123,7 +124,7 @@ LOGIN_URL = "/"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static",]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # MEDIA_URL = 'media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- thirtiethBirthday/settings_dev.py
+++ thirtiethBirthday/settings_dev.py
@@ -5,3 +5,6 @@ SECRET_KEY = 'django-insecure-(t+mex!no50u3&4p@t88!sb@dzq_d31eo12#4&96(n$emk@smx
 DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "TABMOC-v4.local"]
+
+STATIC_ROOT = ""
+STATICFILES_DIRS = [BASE_DIR / "static",]
